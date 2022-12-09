302d6a916ad309bca4f5ffa171f4003f20dbe1ba
saurabh317 <saurabhsp317@gmail.com>
2022-12-09 22:07:23 +0530

    done with the progressbar

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,7 @@
 var progressbar = document.querySelectorAll(".skillProgress > div");
 var skillsContainer = document.getElementById("skills");
 window.addEventListener("scroll", scrollcheck);
+var animationdone = false;
 
 function initialSkillbar(){
     for(let bar of progressbar){
@@ -24,8 +25,16 @@ initialSkillbar();
 
 function fillbar(){
     for(let bar of progressbar){
-        let targetwidth = bar.getAttribute("data-bar-width");
+        let targetwidth = bar.getAttribute('data-bar-width');
         let currentWidth = 0;
+        let interval = setInterval(function (){
+          if(currentWidth > targetwidth){
+            clearInterval(interval);
+            return;
+          }
+         currentWidth++;
+         bar.style.width = currentWidth +"%";
+        } ,7);
     }
 
 }
@@ -34,10 +43,15 @@ function scrollcheck() {
   var ordinates = skillsContainer.getBoundingClientRect();
   console.log(ordinates);
   var innerHeight = window.innerHeight;
-  console.log(innerHeight);
-  if (ordinates.top < innerHeight) {
+  // console.log(innerHeight);
+  if (!animationdone && ordinates.top <= innerHeight) {
+    animationdone = true;
     console.log("skills section is visible");
     fillbar();
+  }
+  else if(ordinates.top > innerHeight){
+    animationdone = false;
+    initialSkillbar();
   }
   
 }
