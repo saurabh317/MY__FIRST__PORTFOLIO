986f5165391e621bf863a5d5d9cc862814f19fb8
saurabh317 <saurabhsp317@gmail.com>
2022-12-09 23:10:13 +0530

    done

--- JS/script.js
+++ JS/script.js
@@ -10,53 +10,52 @@
 
 // }
 // var smoothScroll = setInterval(timer , 50);
-var progressbar = document.querySelectorAll(".skillProgress > div");
-var skillsContainer = document.getElementById("skills");
-window.addEventListener("scroll", scrollcheck);
-var animationdone = false;
 
-function initialSkillbar(){
-    for(let bar of progressbar){
-        bar.style.width = 0 +"%";
-    }
+// ***************script for the skills progress bar****************
+
+var progressbar = document.querySelectorAll(".skillProgress > div");
+
+// ***********before doing anything we need to set the progress to the zero first***************
+
+function initialSkillbar() {
+  for (let bar of progressbar) {
+    bar.style.width = 0 + "%";
+  }
 }
 initialSkillbar();
 
+// ***********now we need to have an eye on the scroll behavior of the window*************
+window.addEventListener("scroll", scrollcheck);
+var animationdone = false;
 
-function fillbar(){
-    for(let bar of progressbar){
-        let targetwidth = bar.getAttribute('data-bar-width');
-        let currentWidth = 0;
-        let interval = setInterval(function (){
-          if(currentWidth > targetwidth){
-            clearInterval(interval);
-            return;
-          }
-         currentWidth++;
-         bar.style.width = currentWidth +"%";
-        } ,7);
-    }
-
-}
-
+var skillsContainer = document.getElementById("skills");
 function scrollcheck() {
   var ordinates = skillsContainer.getBoundingClientRect();
   console.log(ordinates);
   var innerHeight = window.innerHeight;
-  // console.log(innerHeight);
   if (!animationdone && ordinates.top <= innerHeight) {
     animationdone = true;
     console.log("skills section is visible");
     fillbar();
-  }
-  else if(ordinates.top > innerHeight){
+  } else if (ordinates.top > innerHeight) {
     animationdone = false;
     initialSkillbar();
   }
-  
 }
 
+// **********function for fillig up the bar**********
 
-
-
-
+function fillbar() {
+  for (let bar of progressbar) {
+    let targetwidth = bar.getAttribute("data-bar-width");
+    let currentWidth = 0;
+    let interval = setInterval(function () {
+      if (currentWidth > targetwidth) {
+        clearInterval(interval);
+        return;
+      }
+      currentWidth++;
+      bar.style.width = currentWidth + "%";
+    }, 7);
+  }
+}
